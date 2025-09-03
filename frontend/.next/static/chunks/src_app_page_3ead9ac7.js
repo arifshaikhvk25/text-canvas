04893396f3bc7e5df67230884cc2dd0c7d7445ca
arifshaikhvk25 <arifshaikhvk25@gmail.com>
2025-09-03 14:05:23 +0530

    fix: stop tracking .next build output

--- frontend/.next/static/chunks/src_app_page_3ead9ac7.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_ad3352cf._.js",
-  "static/chunks/src_components_SocketTest_1c3e1baf.js"
-],
-    source: "dynamic"
-});
